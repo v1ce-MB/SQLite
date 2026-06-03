fcf415ff75380fed18038a981a4a832d91d2179f
v1ce-MB <mburuvict@gmail.com>
2026-06-03 14:47:45 +0300

    where clause, And clause

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ print("database connected successfully")
 import pandas as pd
 tables = pd.read_sql ("SELECT * FROM sqlite_master WHERE type='table';", conn )
 print(tables)
+
+teams = pd.read_sql("SELECT * FROM Team;", conn)
+print(teams) 
+players = pd.read_sql("SELECT Match_Id, team_id from player_match WHERE Team_id == 8;",conn)
+#print(players)
+GLwins = pd.read_sql("select * from match WHERE Match_winner == 13 AND Season_Id ==9;",conn)
+print(GLwins)
