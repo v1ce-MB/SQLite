69c46982bcd7c85f790403d955af2b5fea35fd63
v1ce-MB <mburuvict@gmail.com>
2026-06-10 14:42:17 +0300

    Alias and sub query

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ example = pd.read_sql("SELECT * FROM example;",conn )
 print(example)
 
 teams = pd.read_sql("SELECT * FROM Team;", conn)
-#print(teams) 
+print(teams) 
 players = pd.read_sql("SELECT Match_Id, team_id from player_match WHERE Team_id == 8;",conn)
 #print(players)
 GLwins = pd.read_sql("select * from match WHERE Match_winner == 13 AND Season_Id ==9;",conn)
@@ -45,7 +45,7 @@ mvps = pd.read_sql ("SELECT DISTINCT (Man_of_the_Match)from Match;", conn)
 ordered_players = pd.read_sql("SELECT * from Player ORDER BY Player_Name DESC;", conn)
 #print(ordered_players)
 #Count is used to get the total numberof rows that satisfy a given condition. 
-GL_total_wins = pd.read_sql("SELECT COUNT(Match_Id) from Match WHERE Match_Winner == 13;", conn)
+GL_total_wins = pd.read_sql("SELECT COUNT(Match_Id) AS GLwins from Match WHERE Match_Winner == 13;", conn)
 #print(GL_total_wins)
 #sum it returns the total sum value of a numerical value
 extra_runs = pd.read_sql("Select sum(Extra_Runs) from Extra_Runs ;", conn)
@@ -54,7 +54,7 @@ extra_runs = pd.read_sql("Select sum(Extra_Runs) from Extra_Runs ;", conn)
 GL_avg_wm = pd.read_sql("SELECT AVG (win_Margin) from Match WHERE Match_Winner == 13;", conn)
 #print(GL_avg_wm)
 # Group_by converts all the summary values in groups of the same values
-result = pd.read_sql("SELECT COUNT (Player_Id), Country_Name  from Player GROUP BY Country_Name;", conn)
+result = pd.read_sql("SELECT COUNT (Player_Id) AS Number_of_Players, Country_Name  from Player GROUP BY Country_Name;", conn)
 #print(result)
 
 
@@ -63,17 +63,17 @@ SELECT *
 FROM example 
 WHERE  age IS NOT NULL;
 """, conn)
-print(null_player_match)
+#print(null_player_match)
 
 #Inner joint only returns records that are in both tables.
 result2 = pd.read_sql ("""
-SELECT Match_Id, Match_Winner, Team_Name
+SELECT Match_Id, Match_Winner, Team_Name AS Match_winner
 FROM match 
 LEFT JOIN Team                     
 ON Match.Match_Winner == Team.Team_Id
 ;
 """ ,  conn)
-print(result2)
+#print(result2)
 
 result3 = pd.read_sql("""
 SELECT Team_Name, Match_Id, Match_Winner
@@ -96,5 +96,26 @@ UNION
 SELECT Name FROM example
 ;
 """ ,conn)
-print(Union)
+#print(Union)
 # Union combines two results that appear from multiple select statements. 
+
+SHMatches_2015 = pd.read_sql("""
+SELECT Match_Id, Team_2 AS Away_Team, Match_Winner
+FROM Match
+WHERE Team_1 IN  
+(SELECT Team_1 FROM Match
+WHERE Team_1 == 11 AND Season_Id == 8 )
+                             ;
+""",conn)
+#print(SHMatches_2015)
+
+results5 = pd.read_sql("""
+SELECT  Season_Id, Man_of_the_Series
+ FROM Season 
+WHERE Man_of_the_Series IN
+(SELECT Player_Id 
+FROM Player
+WHERE Country_Name == 1 )
+;
+""",conn)
+print(results5)
